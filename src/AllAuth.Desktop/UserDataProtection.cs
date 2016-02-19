417cdb67608aac4b0047ee2223e1b82b3d9fede0
Mark Bowker <bowker.mark@gmail.com>
2016-02-19 03:37:55 +0000

    Minor refactor of UserDataProtection class

--- src/AllAuth.Desktop/UserDataProtection.cs
+++ src/AllAuth.Desktop/UserDataProtection.cs
@@ -9,40 +9,44 @@ namespace AllAuth.Desktop
     {
         public static string EncryptData(string dataToEncrypt)
         {
-            if (Environment.OSVersion.Platform != PlatformID.Win32NT)
-            {
-                Logger.Warning("Unsupported platform for user data protection. Using plaintext.");
-                return dataToEncrypt;
-            }
-
             if (dataToEncrypt.Length <= 0)
                 throw new ArgumentException(nameof(dataToEncrypt));
             if (dataToEncrypt == null)
                 throw new ArgumentNullException(nameof(dataToEncrypt));
 
             var bytesToEncrypt = Encoding.UTF8.GetBytes(dataToEncrypt);
-            var encryptedData = ProtectedData.Protect(bytesToEncrypt, null, DataProtectionScope.CurrentUser);
 
-            return Convert.ToBase64String(encryptedData);
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                return Convert.ToBase64String(EncryptDataDpapi(bytesToEncrypt));
+            
+            Logger.Warning("Unsupported platform for user data protection. Using plaintext.");
+            return dataToEncrypt;
+        }
+
+        private static byte[] EncryptDataDpapi(byte[] data)
+        {
+            return ProtectedData.Protect(data, null, DataProtectionScope.CurrentUser);
         }
 
         public static string DecryptData(string dataToDecrypt)
         {
-            if (Environment.OSVersion.Platform != PlatformID.Win32NT)
-            {
-                Logger.Warning("Unsupported platform for user data protection. Using plaintext.");
-                return dataToDecrypt;
-            }
-
             if (dataToDecrypt.Length <= 0)
                 throw new ArgumentException(nameof(dataToDecrypt));
             if (dataToDecrypt == null)
                 throw new ArgumentNullException(nameof(dataToDecrypt));
 
             var bytesToDecrypt = Convert.FromBase64String(dataToDecrypt);
-            var decryptedData = ProtectedData.Unprotect(bytesToDecrypt, null, DataProtectionScope.CurrentUser);
 
-            return Encoding.UTF8.GetString(decryptedData);
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                return Encoding.UTF8.GetString(DecryptDataDpapi(bytesToDecrypt));
+            
+            Logger.Warning("Unsupported platform for user data protection. Using plaintext.");
+            return dataToDecrypt;
+        }
+
+        private static byte[] DecryptDataDpapi(byte[] data)
+        {
+            return ProtectedData.Unprotect(data, null, DataProtectionScope.CurrentUser);
         }
     }
 }
